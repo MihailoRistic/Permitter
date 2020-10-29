13c94964fdfa8eb8c86c61a39a5e45657bbe6035
MFaith <40504666+MihailoRistic@users.noreply.github.com>
2020-10-29 09:10:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 
 const client = new Discord.Client();
 
@@ -7,6 +8,7 @@ const prefix = "p!";
 let binds = new Map();
 
 client.once("ready", () => {
+  binds = new Map(JSON.parse(fs.readFileSync("./data.json")));
   console.log("Online!");
 });
 
@@ -26,36 +28,55 @@ client.on("message", message => {
       if (tc == null) {
         message.channel.send("TC::Pogresno ukucano ili ne postoji :(");
       } else if (tc.type === "text") {
-        message.channel.send("Uspesno spojen " + tc.name + " sa " + vc.name);
-        binds.set(vc, tc);
+        message.channel.send("Uspesno spojen " + vc.name + " sa " + tc.name);
+        binds.set(args[1], args[2]);
+        fs.writeFileSync(
+          "./data.json",
+          JSON.stringify(Array.from(binds.entries()))
+        );
       } else message.channel.send("TC::nije dat tc :(");
     } else message.channel.send("VC::nije dat vc :(");
+  } else if (args[0] === "unbind") {
+    let vc = message.guild.channels.resolve(args[1]);
+    if (vc == null)
+      message.channel.send("VC::Pogresno ukucano ili ne postoji :(");
+    else if (vc.type === "voice")
+      if (!(binds.get(args[1]) == null)) {
+        message.channel.send("Uspesno oslobodjen " + vc.name);
+        binds.delete(args[1]);
+        fs.writeFileSync(
+          "./data.json",
+          JSON.stringify(Array.from(binds.entries()))
+        );
+      } else message.channel.send("VC::nije bio bindovan :(");
+    else message.channel.send("VC::nije dat vc :(");
   }
 });
 
 client.on("voiceStateUpdate", (oldState, newState) => {
   const user = newState.member;
+  const channels = newState.guild.channels;
   let channel = null;
-  if ((channel = binds.get(oldState.channel)) == null)
-    if ((channel = binds.get(newState.channel)) == null) {
+  if ((channel = channels.resolve(binds.get(oldState.channelID))) == null)
+    if ((channel = channels.resolve(binds.get(newState.channelID))) == null) {
     } else
       channel.updateOverwrite(user, {
         VIEW_CHANNEL: true,
         SEND_MESSAGES: newState.mute
       });
-  else if (binds.get(newState.channel) == null)
+  else if (binds.get(newState.channelID) == null)
     channel.updateOverwrite(user, {
       VIEW_CHANNEL: false,
       SEND_MESSAGES: newState.mute
     });
   else {
-    if (channel == binds.get(newState.channel)) {
+    if (channel == channels.resolve(binds.get(newState.channelID))) {
     } else
       channel.updateOverwrite(user, {
         VIEW_CHANNEL: false,
         SEND_MESSAGES: newState.mute
       });
-    binds.get(newState.channel).updateOverwrite(user, {
+    channels.resolve(binds.get(newState.channelID)).updateOverwrite(user, {
       VIEW_CHANNEL: true,
       SEND_MESSAGES: newState.mute
     });
