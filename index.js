07847d4839ab3e08eda747460c18f2f4ebc8f11b
MFaith <40504666+MihailoRistic@users.noreply.github.com>
2020-10-29 03:29:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 const prefix = "p!";
 
-let Ourrole = null;
+let binds = new Map();
 
 client.once("ready", () => {
   console.log("Online!");
@@ -17,28 +17,48 @@ client.on("message", message => {
     .slice(prefix.lenght)
     .split(prefix)[1]
     .split(/ +/);
-  if (args[0] === "setrole") {
-    roleID = message.guild.roles
-      .fetch(args[1])
-      .then(role => {
-        if (role == null) {
-          message.channel.send("Pogresno ukucano ili ne postoji :(");
-        } else {
-          message.channel.send("Uspesno postavljeno na " + role.name + " :)");
-          Ourrole = args[1];
-        }
-      })
-      .catch(console.error);
+  if (args[0] === "bind") {
+    let vc = message.guild.channels.resolve(args[1]);
+    if (vc == null) {
+      message.channel.send("VC::Pogresno ukucano ili ne postoji :(");
+    } else if (vc.type === "voice") {
+      let tc = message.guild.channels.resolve(args[2]);
+      if (tc == null) {
+        message.channel.send("TC::Pogresno ukucano ili ne postoji :(");
+      } else if (tc.type === "text") {
+        message.channel.send("Uspesno spojen " + tc.name + " sa " + vc.name);
+        binds.set(vc, tc);
+      } else message.channel.send("TC::nije dat tc :(");
+    } else message.channel.send("VC::nije dat vc :(");
   }
 });
 
 client.on("voiceStateUpdate", (oldState, newState) => {
-  if (Ourrole == null) {
+  const user = newState.member;
+  let channel = null;
+  let see = null;
+  let write = null;
+  if (newState.channel == null) {
+    console.log("Izasao si iz vc");
+    channel = binds.get(oldState.channel);
+    see = false;
   } else {
-    if (newState.mute || newState.channelID == null)
-      newState.member.roles.add(Ourrole);
-    else newState.member.roles.remove(Ourrole);
+    console.log("Usao si u vc");
+    channel = binds.get(newState.channel);
+    see = true;
   }
+  if (newState.mute) {
+    console.log("Mute se");
+    write = true;
+  } else {
+    console.log("Unmute se");
+    write = false;
+  }
+
+  channel.updateOverwrite(user, {
+    VIEW_CHANNEL: see,
+    SEND_MESSAGES: write
+  });
 });
 
 client.login(process.env.TOKEN);
